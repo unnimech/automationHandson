c80f6ddc546c364cc2bb45421ae946313f741135
unnimech <unni_kct03@yahoo.com>
2021-05-28 22:36:35 -0400

    no message

--- ResearchPaperAutomation/src/test/java/PubMed.java
+++ ResearchPaperAutomation/src/test/java/PubMed.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class PubMed {
 	
 	@Test
-	public static void getAbstractTextContent() {
+	public static void getAbstractText() {
         // declaration and instantiation of objects/variables
     	//System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
 		//WebDriver driver = new FirefoxDriver();
